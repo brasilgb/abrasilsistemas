62589f967627a40f2ac12a6fb649794e6486d7b5
Anderson Brasil <andersonbrasil72@gmail.com>
2026-07-22 09:12:47 -0300

    Push

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        watch: {
+            ignored: [
+                '**/storage/framework/**',
+                '**/vendor/**',
+            ],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
